8a094045c5223a655ecca0fee96c3a55289a65f7
ghanshyam shukla <gsgmca682@gmail.com>
2023-05-06 22:46:15 -0700

    code committed for own kafka docker file

---
+++ demo_test_image/demo_kafka_image/docker/kafka-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create a ZK connection string for the servers and the root.
+ZOOKEEPER_CONNECT=$ZOOKEEPER_SERVERS${ZOOKEEPER_ROOT:=/kafka}
+
+if [ -z $ADVERTISED_HOST_NAME ]; then
+  ADVERTISED_HOST_NAME=$(hostname -f)
+  echo "Advertizing host name ${ADVERTISED_HOST_NAME}."
+fi
+
+# Create the config file.
+sed -i \
+    -e "s|\${ADVERTISED_HOST_NAME}|$ADVERTISED_HOST_NAME|g" \
+    -e "s|\${ZOOKEEPER_CONNECT}|$ZOOKEEPER_CONNECT|g" /kafka/config/server.properties
+
+cd /kafka
+exec "$@"
+Foo
