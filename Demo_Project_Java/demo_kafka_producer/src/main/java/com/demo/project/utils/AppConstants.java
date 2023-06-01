040b9bc076312b01475d435eaad45ccbab1b0caa
ghanshyam shukla <gsgmca682@gmail.com>
2023-06-01 03:44:08 -0700

    code committed for kafka producer helm

--- Demo_Project_Java/demo_kafka_producer/src/main/java/com/demo/project/utils/AppConstants.java
+++ Demo_Project_Java/demo_kafka_producer/src/main/java/com/demo/project/utils/AppConstants.java
@@ -3,4 +3,5 @@ package com.demo.project.utils;
 public class AppConstants {
     public static final String TOPIC_NAME = "javaguides";
     public static final String GROUP_ID = "group_id";
+    public static final String issu="hello";
 }
